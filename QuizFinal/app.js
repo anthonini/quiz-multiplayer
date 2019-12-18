56afb843c0d91ec573075e331b684144d11ad4d2
Anthonini <anthonini@gmail.com>
2019-12-18 06:49:01 -0200

    Config database.
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- QuizFinal/app.js
+++ QuizFinal/app.js
@@ -43,7 +43,7 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
-mongoose.connect('mongodb://localhost/quiz');
+mongoose.connect('mongodb+srv://anthonini:anth12-quizapp@cluster0-nnapr.mongodb.net/test?retryWrites=true&w=majority');
 
 function ensureAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) { return next(); }
